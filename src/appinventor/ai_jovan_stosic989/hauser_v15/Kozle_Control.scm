a72ec5d7f690a4525c53c7ce2227ac3c1d034f54
jstosic <jstosic@t.mk>
2017-07-11 09:54:35 +0200

    First commit

---
+++ src/appinventor/ai_jovan_stosic989/hauser_v15/Kozle_Control.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"159","Source":"Form","Properties":{"$Name":"Kozle_Control","$Type":"Form","$Version":"20","AppName":"hauser","Title":"Hauser Control","Uuid":"0","$Components":[{"$Name":"webKozle_Kontrol","$Type":"WebViewer","$Version":"6","HomeUrl":"http:\/\/ris.mk\/hauser\/hauser.php?ser=%222905970011540122%22","Uuid":"809130254"},{"$Name":"btnHome","$Type":"Button","$Version":"6","Text":"Home","Uuid":"-1983633555"},{"$Name":"TinyDB1","$Type":"TinyDB","$Version":"1","Uuid":"-166929885"}]}}
+|#
---
+++ src/appinventor/ai_jovan_stosic989/hauser_v15/scrRegisterNewHauser.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"159","Source":"Form","Properties":{"$Name":"scrRegisterNewHauser","$Type":"Form","$Version":"20","AppName":"hauser_v11","Title":"Registering a new hauser ...","Uuid":"0","$Components":[{"$Name":"WebViewer1","$Type":"WebViewer","$Version":"6","HomeUrl":"http:\/\/ris.mk\/hauser\/register.php","Uuid":"-433187819"},{"$Name":"btnHome","$Type":"Button","$Version":"6","Text":"Home","Uuid":"1421004808"}]}}
+|#
